f84af1f0e855f74e112b97a32a78c666a04900ef
barryjoya <bariallai@aol.com>
2021-04-29 17:10:42 +0430

    this is template

---
+++ src/main/java/com/amazon/wholefood/com/amazon/wholefood/App.java
@@ -0,0 +1,13 @@
+package com.amazon.wholefood.com.amazon.wholefood;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
